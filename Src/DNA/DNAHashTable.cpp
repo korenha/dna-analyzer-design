77e94b750eda25957de38b5b332f4e8fccdc58d1
Hadasa Koren <z618149961@gmail.com>
2020-07-05 16:47:26 -0700

    Add the class DNAHashTable

--- Src/DNA/DNAHashTable.cpp
+++ Src/DNA/DNAHashTable.cpp
@@ -1,17 +1,18 @@
 #include "DNAHashTable.h"
-void DNAHashTable::insert(const DNAMetaData &data)
+
+void DNAHashTable::insert(const IReader &reader, const std::string &name)
 {
-    DNAData.insert(pair<size_t,DNAMetaData >(id, data));
-    mapByNameToId.insert(pair<std::string,size_t >(data.getName(), id));
-    id++;
+    mapByNameToId.insert(pair<std::string,size_t >(name,id));
+    DNAData.insert(pair<size_t ,DNAMetaData >(id,DNAMetaData(reader,name)));
+    ++id;
 }
 
-size_t DNAHashTable::find(const std::string &name)
-{
-    return mapByNameToId.find(name)->second;
+size_t DNAHashTable::find(const std::string &name)const {
+    return 0;
 }
 
-const DNAMetaData& DNAHashTable::find(size_t _id)
-{
-    return DNAData.find(_id)->second;
-}
+//const DNAMetaData &DNAHashTable::find(size_t _id)const
+//{
+    //return DNAMetaData();
+//}
+
--- Src/DNA/DNAHashTable.h
+++ Src/DNA/DNAHashTable.h
@@ -1,12 +1,26 @@
-//
-// Created by a on 7/5/20.
-//
-
 #ifndef DNAANALYZERPROJECT_DNAHASHTABLE_H
 #define DNAANALYZERPROJECT_DNAHASHTABLE_H
+#include <iostream>
+#include <map>
+#include "DNAMetaData.h"
+using std::pair;
+using std::map;
 
+//using __gnu_cxx::hash_map;
+class DNAHashTable
+{
+public:
+    DNAHashTable():id(1){}
+    void insert(const IReader &reader, const std::string &name);
+    size_t find(const std::string& name)const;
+    const DNAMetaData& find(size_t _id)const;
+    //add functions
+    //void deleteDNa();
+private:
+    size_t id;
+    map<std::string ,size_t > mapByNameToId;
+    map<size_t ,DNAMetaData > DNAData;
 
-class DNAHashTable {
 
 };
 
