3e28b2f57f4170470ee704778074cd9f7030b824
Hadasa Koren <z618149961@gmail.com>
2020-07-06 15:14:31 -0700

    Add the class CommandParsingManager

--- Src/Manager/CommandParsingManager.cpp
+++ Src/Manager/CommandParsingManager.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "CommandParsingManager.h"
+#include "../CML/CommandsParsingFactory.h"
+using std::map;
+
+CML::IParams *CommandParsingManager::getCommand(const std::string& commandName,const std::string &params) {
+    map<std::string,CML::IParams*>::iterator indexCommand = commandParsingMap.find(commandName);
+    if(indexCommand != commandParsingMap.end())
+    {
+        return indexCommand->second;
+    }
+    commandParsingMap.insert(std::pair<std::string,CML::IParams*>(commandName, CML::CommandsParsingFactory::getParams(commandName, params)));
+    return commandParsingMap.find(commandName)->second;}
--- Src/Manager/CommandParsingManager.h
+++ Src/Manager/CommandParsingManager.h
@@ -6,8 +6,15 @@
 #define DNAANALYZERPROJECT_COMMANDPARSINGMANAGER_H
 
 
+#include <string>
+#include "../CML/IParams.h"
+#include <map>
 class CommandParsingManager {
-
+public:
+    CML::IParams* getCommand(const std::string& commandName,const std::string &command);
+    //add dtor
+private:
+    static std::map<std::string,CML::IParams*> commandParsingMap;
 };
 
 
