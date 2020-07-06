09fd43033818004af1436228e8312662aae6d884
Hadasa Koren <z618149961@gmail.com>
2020-07-06 12:35:07 -0700

    Add the class CommandName and using it in the Factories classes

--- Src/CML/CommandName.cpp
+++ Src/CML/CommandName.cpp
@@ -4,12 +4,12 @@
 
 #include "CommandName.h"
 namespace CML{
-    std::string commandsName[CommandName::E_Last] = {};
+    std::string commandsName[CommandName::E_Last+1] = {"new"};
 
     CommandName::ECommand CommandName::getECommand(const std::string &commandName)
     {
         unsigned int indexCommand;
-        for (indexCommand = 0; indexCommand < E_Last &&  commandName != commandsName[indexCommand]; ++indexCommand);
+        for (indexCommand = 0; indexCommand < E_Last+1 &&  commandName != commandsName[indexCommand]; ++indexCommand);
         return (ECommand)indexCommand;
 
     }
