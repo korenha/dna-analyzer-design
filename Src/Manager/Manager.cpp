d8eb43011cd36033399bdfd7a8bbc8958c1df622
Hadasa Koren <z618149961@gmail.com>
2020-07-07 06:24:59 -0700

    rewrite the functio doAction

--- Src/Manager/Manager.cpp
+++ Src/Manager/Manager.cpp
@@ -1,69 +1,64 @@
 #include "Manager.h"
-#include "../Reader/ConsulReader.h"
-#include "../Writer/ConsulWriter.h"
+
 #include <iostream>
 #include "../Exception/MyException.h"
 #include "CommandsManager.h"
 #include "CommandParsingManager.h"
 #include "../CML/IParams.h"
 #include <sstream>
-static void removeTheInputBeginningAndCheckItsSyntax(std::string& input);/*> cmd >>>*/
-static std::string getNameCommandAndRemoveFromTheInput(std::string& input);
+#include <vector>
+
+static std::vector<std::string> parsingCommand(const std::string& input);
+static void checkValidCommandStart(const std::vector<std::string> &params);/*> cmd >>>*/
 
 void Manager::doAction()
 {
     std::cout<<"|--------Start Run-------------|"<<std::endl;
-    ConsulReader  reader;
-    ConsulWriter writer;
-    std::string input;
-    input = reader.read();
+    std::string input,output;
+    input = m_reader->read();
     CommandsManager commandsManager;
     CommandParsingManager commandParsingManager;
     try
     {
-        removeTheInputBeginningAndCheckItsSyntax(input);
-        std::string commandName = getNameCommandAndRemoveFromTheInput(input);
-        commandsManager.getCommand(commandName)->run((CML::IParams*)commandParsingManager.getParams(commandName,input));
+        std::vector<std::string> params = parsingCommand(input);
+        checkValidCommandStart(params);
+        output = commandsManager.getCommand(params[3])->run((CML::IParams*)commandParsingManager.getParams(params[3],std::vector<std::string>(params.begin()+4,params.end())));
+        m_writer->write(output.c_str());
         input.clear();
     }
     catch (const MyException& e)
     {
-        writer.write(e.what());
+        m_writer->write(e.what());
     }
 
 }
 
-static void removeTheInputBeginningAndCheckItsSyntax(std::string& input)
+static void checkValidCommandStart(const std::vector<std::string> &params)
 {
-    size_t i;
-    for (i = 0; i < input.size() && input[i] == ' '; ++i);
-    if(input[i] != '>')
+
+    if(params[0] != ">")
     {
         throw MyException("SyntaxError:  Expected get '>' ");
     }
-    ++i;
-    for (; i < input.size() && input[i] == ' '; ++i);
-    if(input.find("cmd") != i)
+    if(params[1] != "cmd")
     {
         throw MyException("SyntaxError: Expected get 'cmd' ");
     }
-    i += 3;
-    for (; i < input.size() && input[i] == ' '; ++i);
-    if(input.find(">>>") != i)
+    if(params[2] != ">>>")
     {
         throw MyException("SyntaxError: Expected get '>>>' ");
     }
-    i += 3;
-    for (; i < input.size() && input[i] == ' '; ++i);
-    input = std::string(input.begin() + i,input.end());
-
 }
 
-static std::string getNameCommandAndRemoveFromTheInput(std::string& input)
+static std::vector<std::string> parsingCommand(const std::string& input)
 {
-    size_t index=input.find(' ');
-    std::string commandName = std::string(input.begin(),input.begin() + index);
-    for (; index < input.size() && input[index] == ' '; ++index);
-    input = std::string(input.begin()+index,input.end());
-    return commandName;
+    std::vector<std::string> parsedVector;
+    size_t wordBegin = 0;
+    for (size_t wordEnd = 0; wordEnd < input.size(); ++wordEnd)
+    {
+        for (wordBegin = wordEnd; wordBegin < input.size() && input[wordBegin] == ' '; ++wordBegin);
+        for (wordEnd = wordBegin; wordEnd < input.size() && input[wordEnd] != ' '; ++wordEnd);
+        parsedVector.push_back(std::string(input.begin()+wordBegin,input.begin() + wordEnd));
+    }
+    return parsedVector;
 }
--- Src/Manager/Manager.h
+++ Src/Manager/Manager.h
@@ -2,10 +2,20 @@
 #define DNAANALYZERPROJECT_MANAGER_H
 
 
+#include "../Reader/IReader.h"
+#include "../Writer/IWriter.h"
+#include "../Reader/ConsulReader.h"
+#include "../Writer/ConsulWriter.h"
 class Manager
 {
 public:
-    static void doAction();
+    void doAction();
+    Manager(IReader* reader = new ConsulReader(),IWriter* writer = new ConsulWriter()):m_reader(reader),m_writer(writer){}
+    ~Manager(){delete m_reader;delete m_writer;}
+
+private:
+    IReader* m_reader;
+    IWriter* m_writer;
 };
 
 
